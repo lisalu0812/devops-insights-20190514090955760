c86549607cd2286c5ac5fb1d43dab203676095b7
Lisa Lu <luyj0812@gmail.com>
2019-05-14 22:33:11 +0000

    Updating codes

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -95,7 +95,7 @@
           }
         ],
         main: {
-          temp: 78
+          temp: 25.5
         }
       };
 
@@ -182,7 +182,7 @@
           }
         ],
         main: {
-          temp: 78
+          temp: 25.5
         }
       };
 
@@ -196,7 +196,7 @@
 
       assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
       assert(resMock.send.lastCall.args[0].city === 'Hamilton', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
-      assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 78 F', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
+      assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 25.5 C', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
     });
   });
   

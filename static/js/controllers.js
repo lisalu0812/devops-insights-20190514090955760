53e5c87d535673b3cce6ef83ae62fe597d4d8f2d
Lisa Lu <luyj0812@gmail.com>
2019-05-16 08:48:48 +0000

    Adjusting map

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -22,9 +22,8 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     var map = new google.maps.Map(
         document.getElementById('map'), {zoom:6, center: auckland});
     var marker = new google.maps.Marker ({position: auckland, map: map});
-    //
-    //var places = [];
-      //Adding a latlng obj for each city
+ 
+      //Adding more markers
       var ashburton = {lat:-43.89834, lng: 171.73011};
       var marker1 = new google.maps.Marker ({position: ashburton, map: map});
       var christchurch = {lat:-43.53333, lng: 172.63333};
@@ -43,24 +42,8 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
       var marker8 = new google.maps.Marker ({position: palmerston_north, map: map});
       var wellington = {lat:-41.28664,lng:174.77557};
       var marker9 = new google.maps.Marker ({position: wellington, map: map});
-      //places.push(ashburton);
-      //places.push(new google.maps.LatLng(-43.89834, 171.73011));  //Ashburton
-      /*places.push(new google.maps.LatLng(-43.53333, 172.63333));  //Christchurch
-      places.push(new google.maps.LatLng(-37.78333, 175.28333));  //Hamilton
-      places.push(new google.maps.LatLng(-45.87416, 170.50361));  //Dunedin
-      places.push(new google.maps.LatLng(-41.27078, 173.28404));  //Nelson
-      places.push(new google.maps.LatLng(-39.06667, 174.08333));  //New Plymouth
-      places.push(new google.maps.LatLng(-45.03023, 168.66271));  //Queenstown
-      places.push(new google.maps.LatLng(-40.35636, 175.61113));  //Palmerston North
-      places.push(new google.maps.LatLng(-41.28664, 174.77557));  //Wellington
-      */
-     //Loop through the places array
-     /*for (var i=0; i< places.length;i++){
-     	//Create a new marker
-     	var m = new google.map.Marker ({
-     		position: places[i],map: map
-     	});
-     }*/
+     
+      
     $scope.zip = function(which) {
 
         var data = "";

98f10c6bd4a176231aebcffaded095581993fa50
Lisa Lu <luyj0812@gmail.com>
2019-05-16 06:35:09 +0000

    Adding an array of markers

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -19,9 +19,27 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
 
     var auckland = {lat: -36.848461, lng: 174.763336};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom:4, center: auckland});
+        document.getElementById('map'), {zoom:6, center: auckland});
     var marker = new google.maps.Marker ({position: auckland, map: map});
-    
+    var places = [];
+      //Adding a latlng obj for each city
+      places.push(new google.maps.LatLng(-43.89834, 171.73011));  //Ashburton
+      places.push(new google.maps.LatLng(-43.53333, 172.63333));  //Christchurch
+      places.push(new google.maps.LatLng(-37.78333, 175.28333));  //Hamilton
+      places.push(new google.maps.LatLng(-45.87416, 170.50361));  //Dunedin
+      places.push(new google.maps.LatLng(-41.27078, 173.28404));  //Nelson
+      places.push(new google.maps.LatLng(-39.06667, 174.08333));  //New Plymouth
+      places.push(new google.maps.LatLng(-45.03023, 168.66271));  //Queenstown
+      places.push(new google.maps.LatLng(-40.35636, 175.61113));  //Palmerston North
+      places.push(new google.maps.LatLng(-41.28664, 174.77557));  //Wellington
+      
+     //Loop through the places array
+     for (var i=0; i< places.length;i++){
+     	//Create a new marker
+     	var marker = new google.map.Marker ({
+     		position: places[i],map: map
+     	});
+     }
     $scope.zip = function(which) {
 
         var data = "";

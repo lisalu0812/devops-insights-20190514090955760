cf70656b65d39e6f88ff72812c4dce25233b4c6e
Lisa Lu <luyj0812@gmail.com>
2019-05-16 17:26:32 +0000

    Updating codes

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -94,7 +94,8 @@
     			else{
     				assert.equal(resp.statusCode,200);
     				var pbody = JSON.parse(body);
-    				assert(pbody.city==='Auckland',"City name does not match")
+    				assert(pbody.city==='Auckland',"City name does not match");
+    				done();
     			}
     		});
     	});

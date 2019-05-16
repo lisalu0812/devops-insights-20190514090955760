9e88a5b84604fc8dd90db08fdae60d1d72839319
Lisa Lu <luyj0812@gmail.com>
2019-05-16 04:18:38 +0000

    Updating map

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -17,6 +17,11 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     $scope.zip1City = "";
     $scope.zip1Weather = "";
 
+    var auckland = {lat: -36.848461, lng: 174.763336};
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom:4, center: auckland});
+    var marker = new google.maps.Marker ({position: auckland, map: map});
+    
     $scope.zip = function(which) {
 
         var data = "";
